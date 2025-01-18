6bad10ded6b901c93a186d6a13abd13b79e0818e
krishadhola <dholakrisha9099@gmail.com>
2025-01-18 16:06:57 +0000

    container config done

---
+++ 04_basic/01_function.js
@@ -0,0 +1,31 @@
+
+
+function myName()
+{
+    console.log("K");
+    console.log("R");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+}
+// myName()
+
+// function NumAdd(Number1,Number2)
+// {
+//     console.log(Number1+Number2);
+    
+// }
+// NumAdd(3,4)
+
+function NumAdd(Number1,Number2)
+{
+        let result= Number1+Number2
+        return result             //any atatement not processed after return keyword
+}
+  const result=NumAdd(3,5)
+  console.log("result",result);
+  
+
+
+
