ca296b567c6be5107d6cec07f2cb9ccc151b04dd
krishadhola <dholakrisha9099@gmail.com>
2025-01-20 06:42:38 +0000

    container config done

--- 04_basic/01_function.js
+++ 04_basic/01_function.js
@@ -32,5 +32,50 @@ function NumAdd(Number1,Number2)
   // console.log("result",result);
   
 
+  function LoggedInUser(UserName = "sam")
+  {
+    // if(UserName==undefined | !UserName) 
+    // {
+    //   console.log("please enter username");
+      
+    // }
+      return `${UserName} is just logged in`
+  }
+  // console.log(LoggedInUser("krisha"));
+  // console.log(LoggedInUser());  // it return undefined if argument is not passed
+
+  function calculateCartItem( val1, val2, ...num1)  // ...(rest operator) collect all argument or values
+  {
+    return num1
+  }
+  // console.log(calculateCartItem(200,400,300,500));
 
 
+  // **************************how the object is passed through function and use it ********************************
+
+  const user=
+  {
+    // username : "hitesh",
+    // prices : 9999
+  }
+
+  function handleUser(anyObject)
+  {
+    // console.log(`username is ${anyObject.username} and price is ${anyObject.prices}`);
+    
+  }
+  handleUser({
+    
+    username : "hitesh",
+    prices : 9999
+  })
+
+//with array
+
+const myArray=[200,700,500,900]
+
+function returnSecondValue(getArray)
+{
+   return getArray[2]
+}
+console.log(returnSecondValue(myArray));
