7dbd06d2bb1218daa3e185bacac7e418a742374a
krishadhola <dholakrisha9099@gmail.com>
2025-01-23 07:35:59 +0000

    container config done

---
+++ 05_iteration/five.js
@@ -0,0 +1,46 @@
+// for each loop
+
+const coding=["c++","java","python","js","ruby"]
+
+// coding.forEach(function (items) {
+//     console.log(items);
+    
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item)
+// })
+
+// function printMe(items)
+// {
+//     console.log(items);
+    
+// }
+
+// coding.forEach(printMe)
+
+
+// coding.forEach((val,index,arr)=>{
+//     console.log(val,index,arr);
+    
+// })
+
+const myCodding=[
+    {
+        name : "krisha",
+        language : "gujarati"
+    },
+    {
+        name : "archi",
+        language : "english"
+    },
+    {
+        name : "dhruv",
+        language : "hindi"
+    }
+]
+
+myCodding.forEach((item)=>{
+    console.log(item.name);
+    
+})
